8324be874e11b657e79f5989aa86e518a934be64
Jesper Friis <jesper.friis@sintef.no>
2021-04-23 11:42:39 +0200

    Import from web instead of local files

--- dic2owl/dic2owl/__init__.py
+++ dic2owl/dic2owl/__init__.py
@@ -1,3 +1,6 @@
 __version__ = "0.1.0"
 __author__ = "Jesper Friis"
 __author_email__ = "jesper.friis@sintef.no"
+
+# Where versioned releases can be downloaded from
+release_site = 'https://raw.githubusercontent.com/emmo-repo/CIF-ontology/gh-pages/versions'
